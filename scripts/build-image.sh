ac0d9f5e72f19b6478433d50aa851a0862f76ee5
Roy97 <s224021028@deakin.edu.au>
2024-09-20 17:38:41 +1000

    terraform integration

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -6,7 +6,6 @@ set -u
 IMAGE1=pavan-jarjana-sit722-part5-book_catalog
 IMAGE2=pavan-jarjana-sit722-part5-inventory_management
 
-cd ..
 cd book_catalog
 docker build -t $CONTAINER_REGISTRY/$IMAGE1:$VERSION --file Dockerfile .
 cd ..
