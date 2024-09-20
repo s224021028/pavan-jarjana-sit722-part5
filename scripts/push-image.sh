84dfbbf66617272140e5b985fc7147636a1bdda3
Roy97 <s224021028@deakin.edu.au>
2024-09-20 17:20:44 +1000

    terraform integration

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -2,12 +2,9 @@
 set -u
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
-: "$REGISTRY_USERNAME"
-: "$REGISTRY_PASSWORD"
 
 IMAGE1=pavan-jarjana-sit722-part5-book_catalog
 IMAGE2=pavan-jarjana-sit722-part5-inventory_management
 
-echo $REGISTRY_PASSWORD | docker login $CONTAINER_REGISTRY --username $REGISTRY_USERNAME --password-stdin
 docker push $CONTAINER_REGISTRY/$IMAGE1:$VERSION
 docker push $CONTAINER_REGISTRY/$IMAGE2:$VERSION
